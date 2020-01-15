c7488beb9273580aa4659b72c725340b0e67d538
Joe <joeschwehr@gmail.com>
2020-01-14 19:53:20 -0800

    Updates

---
+++ precache-manifest.f7b987f4b58887645856aad336feb11a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b383dda0b7d8a5c1a1adf908c919f9d",
+    "url": "/d3-react-scatterplot/index.html"
+  },
+  {
+    "revision": "0af2833121da1bc89ec7",
+    "url": "/d3-react-scatterplot/static/css/main.591d9824.chunk.css"
+  },
+  {
+    "revision": "ae55e5a312e6df247e16",
+    "url": "/d3-react-scatterplot/static/js/2.18cfe248.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/d3-react-scatterplot/static/js/2.18cfe248.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0af2833121da1bc89ec7",
+    "url": "/d3-react-scatterplot/static/js/main.8d94d553.chunk.js"
+  },
+  {
+    "revision": "2cc3221ee4d6998de318",
+    "url": "/d3-react-scatterplot/static/js/runtime-main.5c3bf06e.js"
+  }
+]);
